5abdfe73434239da4e2c69d140295eb4b10428ca
Соkrikaru <lesoleill64@gmail.com>
2021-02-28 22:28:33 +0300

    add repository for user and messages

---
+++ src/main/java/org/example/springBoot/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package org.example.springBoot.repos;
+
+import org.example.springBoot.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
