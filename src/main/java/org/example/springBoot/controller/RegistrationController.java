364ef40e8760cce87008a34b8f8ed7b7857272d1
Соkrikaru <lesoleill64@gmail.com>
2021-09-09 12:10:46 +0300

    add email of users, send activation code to email

--- src/main/java/org/example/springBoot/controller/RegistrationController.java
+++ src/main/java/org/example/springBoot/controller/RegistrationController.java
@@ -6,7 +6,9 @@ import org.example.springBoot.repos.UserRepo;
 import org.example.springBoot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
@@ -31,7 +33,17 @@ public class RegistrationController {
             return "registration";
         }
 
-
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activateUser(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated!");
+        }else {
+            model.addAttribute("message", "Activation code isn't found!");
+        }
+        return "login";
     }
 }
