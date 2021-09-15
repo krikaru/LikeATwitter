ccaf3fb33b7c506386922df07570f6242e9fdb22
Соkrikaru <lesoleill64@gmail.com>
2021-09-15 17:35:02 +0300

    add editor of message

--- src/main/java/org/example/springBoot/domain/User.java
+++ src/main/java/org/example/springBoot/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -22,14 +23,15 @@ public class User implements UserDetails {
     @NotBlank(message = "Password cannot be empty")
     private String password;
 
-
-
     private boolean active;
 
     @Email(message = "Email is not correct")
     @NotBlank(message = "Email cannot be empty")
     private String email;
     private String activationCode;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Message> messages;
 
     //говорит о том, что это элементы коллекции, указывая класс. Фетч - тип подгрузки:жадный.
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -125,5 +127,24 @@ public class User implements UserDetails {
         this.activationCode = activationCode;
     }
 
+    public Set<Message> getMessages() {
+        return messages;
+    }
 
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
