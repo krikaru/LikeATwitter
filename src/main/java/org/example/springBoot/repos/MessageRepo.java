e6e9cf9c20b338014cdb7d29c415cc4a00f6f301
Соkrikaru <lesoleill64@gmail.com>
2021-10-04 19:10:18 +0300

    add pagination

--- src/main/java/org/example/springBoot/repos/MessageRepo.java
+++ src/main/java/org/example/springBoot/repos/MessageRepo.java
@@ -1,11 +1,13 @@
 package org.example.springBoot.repos;
 
 import org.example.springBoot.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    List<Message> findByTag(String tag);
+    Page<Message> findAll(Pageable pageable);
+
+    Page<Message> findByTag(String tag, Pageable pageable);
 }
